0d7b2f35196df540bde3b1d3e7a10cd44182fdd9
eugenechia95 <eugenechia95@gmail.com>
2023-12-14 18:08:23 +0800

    feat: toggle color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import { Button, createTheme, ThemeProvider } from '@mui/material'
-import { lime, orange, purple } from '@mui/material/colors'
+import { Box, Button, Container, createTheme, ThemeProvider, useTheme } from '@mui/material'
+import { amber, deepOrange, grey, lime, orange, purple } from '@mui/material/colors'
 import Search from './components/Search/Search'
 
 declare module '@mui/material/styles' {
@@ -18,25 +18,74 @@ declare module '@mui/material/styles' {
   }
 }
 
-const theme = createTheme({
-  status: {
-    danger: orange[500],
-  },
-  palette: {
-    primary: purple,
-    secondary: lime,
-  },
-})
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    backgroundImage: string
+  }
+}
+
+const ColorModeContext = React.createContext(() => {})
 function App() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light')
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+  }
+
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+    palette: {
+      mode,
+      ...(mode === 'light'
+        ? {
+            // palette values for light mode
+            primary: amber,
+            divider: amber[200],
+            backgroundImage: 'url(/bg-light.png)',
+            text: {
+              primary: grey[900],
+              secondary: grey[800],
+            },
+          }
+        : {
+            // palette values for dark mode
+            primary: deepOrange,
+            divider: deepOrange[700],
+            backgroundImage: 'url(/bg-dark.png)',
+            background: {
+              default: deepOrange[900],
+              paper: deepOrange[900],
+            },
+            text: {
+              primary: '#fff',
+              secondary: grey[500],
+            },
+          }),
+    },
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <div className='App'>
-        <Button variant='contained'>Primary</Button>
-        <div>
-          <Search />
-        </div>
-      </div>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={toggleColorMode}>
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            backgroundImage: theme.palette.backgroundImage,
+            height: '100vh',
+            backgroundSize: 'cover',
+          }}
+        >
+          <Container>
+            <Button variant='contained' onClick={toggleColorMode}>
+              Toggle Theme
+            </Button>
+            <div>
+              <Search />
+            </div>
+          </Container>
+        </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
